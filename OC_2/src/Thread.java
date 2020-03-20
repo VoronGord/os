7ad78296d67c667db705fae4d3f2010fc656b0d7
VoronGord <56074431+VoronGord@users.noreply.github.com>
2020-03-20 12:36:48 +0400

    Update Thread.java
    ver2

--- OC_2/src/Thread.java
+++ OC_2/src/Thread.java
@@ -1,4 +1,35 @@
 
+
+
 public class Thread {
+	final int time;
+
+	private int ostTime;
+
+	private int name;
+
+	public Thread(int name) {
+		this.name = name;
+		time = (int) (Math.random() * 10) + 5;
+		ostTime = time;
+	}
+
+	public int getName() {
+		return name;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public int getOstTime() {
+		return ostTime;
+	}
+
+	public void setOstTime(int time) {
+		ostTime = time;
+	}
 
 }
+
+
