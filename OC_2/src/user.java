ae1c4611ed00f3154059528dcac7b3d64c53a0ca
VoronGord <56074431+VoronGord@users.noreply.github.com>
2020-03-20 12:37:15 +0400

    Update user.java
    ver2

--- OC_2/src/user.java
+++ OC_2/src/user.java
@@ -1,14 +1,18 @@
 
-public class user {
-	private static process process = new process();
-	public static final int alltime = 20;
-	
-	public static void main (String[] args) {
-		
-		System.out.println("Запускаем программу");
-		System.out.println("Отведено времени на выполнение процесса: " + alltime + "мс");
-		process.CreateProcess((int)(Math.random()*5) +1);
-		process.CheckProcess();
-	}
+import java.util.ArrayList;
 
+
+
+public class user {
+	public static void main(String[] args) {
+		ArrayList<process> process = new ArrayList<process>();
+		int k = (int) (Math.random() * 4) + 1;
+		for (int i = 0; i < k; i++) {
+			process.add(new process());
+			int n = i + 1;
+			process.get(i).setName(n + "");
+		}
+		Planner scheduler = new Planner(process);
+		System.out.println(scheduler.makePlan());
+	}
 }
